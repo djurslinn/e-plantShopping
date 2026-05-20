7b54993bb460c4c72b586ca369f5e595f9c649b7
djurslinn <djurslinnjameskm@gmail.com>
2026-05-20 17:13:19 +0530

    added product list display

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,45 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+  reducers: {
+
+    // ADD ITEM
+    addItem: (state, action) => {
+      const existingItem = state.items.find(
+        item => item.name === action.payload.name
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+
+    // REMOVE ITEM
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        item => item.name !== action.payload.name
+      );
+    },
+
+    // UPDATE QUANTITY
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+
+      const item = state.items.find(
+        item => item.name === name
+      );
+
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
